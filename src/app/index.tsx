ba760b70e01e1786f88e49359f1b090f44239a5a
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-01-20 18:09:19 -0300

    feat: spinner in index middleware route and lib for transform date

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,21 @@
+import { Spinner } from "@/components/ui/Spinner";
+import { useAuth } from "@/hooks/auth.hook";
 import { Redirect } from "expo-router"
 
 
 
+
+
 export default function Index(){
+  const { token, isLoading } = useAuth() 
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
+  if(token){
+    return <Redirect href="/(tabs)" />;
+  }
+
   return <Redirect href="/Login" />;
 }
