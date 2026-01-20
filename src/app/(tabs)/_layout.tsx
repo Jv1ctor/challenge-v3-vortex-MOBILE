bdfde6dd3bea1ff7cba323e33b6d325172c7b4a6
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-01-20 18:05:35 -0300

    feat: add tabs and home list machine

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Tabs } from "expo-router"
+import Feather from '@expo/vector-icons/Feather';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function TabsLayout(){
+
+  return(
+    <Tabs screenOptions={{ tabBarActiveTintColor: "#1d6bf5" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Maquinas",
+          tabBarIcon: ({ color }) => <Feather name="home" size={24} color={color} />
+        }}
+      />
+      
+      <Tabs.Screen
+        name="perfil"
+        options={{
+          title: "Perfil",
+          tabBarIcon: ({ color }) => <Ionicons name="person" size={24} color={color} />
+        }}
+      />
+      
+    </Tabs>
+  )
+}
---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { CardMachine } from "@/components/CardMachine"
+import { Text, View } from "react-native"
+
+export default function Home (){
+  return(
+    <View>
+      <Text>Fabrica ...</Text>
+      <CardMachine />
+    </View>
+  )
+}
